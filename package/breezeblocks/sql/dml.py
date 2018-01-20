e6f0e8b8601d01f76623d11b6d3b72fd0023c5d9
modimore <quinn.e.mortimer@gmail.com>
2018-01-20 16:21:02 -0500

    Consolidating DML functionality into DML modules
    Insert, Update, and Delete operations are in `breezeblocks.sql.dml`.
    Insert, Update, and Delete builders are in `breezeblocks.dml_builders`.
    
    DML functionality is not quite as involved as query functionality,
    and this feels correct for module organization at least right now.

--- package/breezeblocks/sql/dml.py
+++ package/breezeblocks/sql/dml.py
@@ -1,5 +1,4 @@
-from ..exceptions import InsertError
-
+from ..exceptions import InsertError, UpdateError, DeleteError
 from .query import Query
 
 class Insert(object):
@@ -80,3 +79,61 @@ class Insert(object):
             cur.executemany(statement, data)
             cur.close()
             conn.commit()
+
+class Update(object):
+    """Represents a database update."""
+    
+    def __init__(self, statement, params, db=None):
+        """Initializes an update statement against a specific database.
+        
+        :param statement: The SQL statement for the update.
+        :param params: A list of literal values to pass into the statement.
+        :param db: The database to perform the update on.
+        """
+        if db is None:
+            raise UpdateError('Attempting to update without a database.')
+        
+        self._db = db
+        self._statement = statement
+        self._params = params
+    
+    def execute(self):
+        """Executes the update in the database."""
+        with self._db.pool.get() as conn:
+            cur = conn.cursor()
+            cur.execute(self._statement, self._params)
+            cur.close()
+            conn.commit()
+    
+    def show(self):
+        """Show the constructed SQL statement for this update."""
+        print(self._statement, self._params, sep='\n')
+
+class Delete(object):
+    """Represents a database delete."""
+    
+    def __init__(self, statement, params, db=None):
+        """Initializes a delete statement against a specific database.
+        
+        :param statement: The SQL statement for the delete.
+        :param params: A list of literal values to pass into the statement.
+        :param db: The database to perform the delete on.
+        """
+        if db is None:
+            raise DeleteError('Attempting to delete without a database.')
+        
+        self._db = db
+        self._statement = statement
+        self._params = params
+    
+    def execute(self):
+        """Executes the delete in the database."""
+        with self._db.pool.get() as conn:
+            cur = conn.cursor()
+            cur.execute(self._statement, self._params)
+            cur.close()
+            conn.commit()
+    
+    def show(self):
+        """Show the constructed SQL statement for this delete."""
+        print(self._statement, self._params, sep='\n')
